f0c44c789e8b4b24deed1ccb28e868790a666c2f
Paul Onawola <onawola.ire@gmail.com>
2023-10-05 10:39:33 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Paul Onawola on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
